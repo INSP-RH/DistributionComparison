ef44b14edb36ad691dd623979bd16c1702573419
Dalia Camacho García Formentí <daliaf172@gmail.com>
2017-04-21 17:37:03 -0500

    Add files via upload

--- Distributions_vs_Normal.R
+++ Distributions_vs_Normal.R
@@ -1,5 +1,5 @@
 #Distribution comparison
-#María José Benítez / Rodrigo Zepeda / Dalia Camacho García
+#Mar?a Jos? Ben?tez / Rodrigo Zepeda / Dalia Camacho Garc?a
 #INSP 2017
 
 #Description
@@ -25,27 +25,31 @@ Fnorm <- function(x){pnorm(x, mean(pop), sd(pop))}
 #Create data frame for plot
 plotdata <- data.frame(t, Freal(t), Fnorm(t), 
                        Freal(t) - Fnorm(t))
-colnames(plotdata) <- c("t","Real","Normal","Error_Normal")
+colnames(plotdata) <- c("t","Real","Normal","Error")
 plotdata$Cumerror <- abs(plotdata$Error)
 for (i in 2:length(t)){
-  plotdata$Cumerror[i] <- abs(plotdata$Error) +  
+  plotdata$Cumerror[i] <- abs(plotdata$Error)[i] +  
     plotdata$Cumerror[i-1]
 }
+plotdata$minAccumulatedError <- sapply(t, function(x){minAccumulatedError(x,pop,"norm")})
+plotdata$maxAccumulatedError <- sapply(t, function(x){maxAccumulatedError(x,pop,"norm")})
 
 #Plot the distributions
 plot1 <- ggplot(plotdata, aes(x = t)) + 
-            geom_line(aes(y = abs(Error)), color = "forestgreen") +
-            ggtitle("Absolute point error of real vs normal") +
-            theme_bw()
+  geom_line(aes(y = abs(Error)), color = "forestgreen") +
+  ggtitle("Absolute point error of real vs normal") +
+  theme_bw()
 plot2 <- ggplot(plotdata, aes(x = t)) + 
-            geom_line(aes(y = Cumerror), color = "orange") +
-            ggtitle("Cummulative point error of real vs normal") +
-            theme_bw()
+  geom_line(aes(y = Cumerror, color = "Accumulated Error")) +
+  geom_line(aes(y = minAccumulatedError, color = "Minimum Accumulated")) +
+  geom_line(aes(y = maxAccumulatedError, color = "Maximum Accumulated")) +
+  ggtitle("Cummulative point error of real vs normal") +
+  theme_bw()
 plot3 <- ggplot(plotdata, aes(x = t)) + 
-            geom_step(aes(y = Real), color = "red") +
-            geom_line(aes(y = Normal), color = "deepskyblue4") +
-            theme_bw() +
-            ggtitle("Real vs Normal")
+  geom_step(aes(y = Real), color = "red") +
+  geom_line(aes(y = Normal), color = "deepskyblue4") +
+  theme_bw() +
+  ggtitle("Real vs Normal")
 
 grid.arrange(plot1, plot2, plot3)
 
---
+++ maxAccumulatedError.R
@@ -0,0 +1,85 @@
+# The function maxAccumulatedError calculates the maximum accumulated error between the real cummulative distribution 
+# for a finite population compared and the cummulative distribution assuming a parametric distribution
+# the parametric distribution takes its parameters from the whole distribution. 
+# The function receives a value y at which the accumulated error is calculated, the values for each individual in the
+# population and the assumed parametric distribution, supports normal, lognormal and gamma.
+maxAccumulatedError <- function(y, pop, distribution="norm"){
+  M        <- length(pop) # Population size
+  pop      <- sort(pop)   # Order population
+  Xm       <- pop[M]      # Largest value in the population
+  X0       <- pop[1]      # Smallest value
+  
+  
+  # Choose integral of the parametric cummulative distribution
+  switch(distribution,
+         "norm" = {
+           mupop    <- mean(pop)
+           sdpop    <- sd(pop)
+           integral <- function(a,b){integrate(function(t){pnorm(t,mupop,sdpop)}, a, b)}
+         },
+         "lnorm"= {
+           mupop    <- mean(pop)
+           varpop   <- var(pop)
+           
+           Param.log<-function(mux,varx){
+             sd2     <- log(varx/(mux^2)+1)
+             meanlog <- log(mux)-1/2*sd2
+             sdlog   <- sqrt(sd2)
+             return(c(meanlog,sdlog))
+           }
+           params   <- Param.log(mupop, varpop)
+           integral <- function(a,b){integrate(function(t){plnorm(t,params[1],params[2])}, a, b)}
+         },
+         "gamma"= {
+           mupop    <- mean(pop)
+           varpop   <- var(pop)
+           
+           Param.gamma<-function(mux,varx){
+             alfa     <- mux^2/varx
+             beta     <- alfa/mux
+             return(c(alfa,beta))
+           }
+           params   <- Param.gamma(mupop, varpop)
+           integral <- function(a,b){integrate(function(t){pgamma(t,params[1],params[2])}, a, b)}
+         }
+  )
+  
+  # Accumulated error for values smaller than X0
+  if(y<X0){
+    firstIntegral <- as.numeric(integral(-Inf,y)[1])
+    return(firstIntegral)
+  }
+  
+  tabpop   <- table(pop)
+  ivec     <- sort(unique(pop))
+  ivec     <- ivec[which(ivec<=y)]
+  j        <- min(max(ivec),M)
+  
+  # Calculate the integral from -Inf,j
+  firstIntegral <- as.numeric(integral(-Inf, j)[1])
+  
+  # Calculate the sum of the cummulative empirical function from X0 to values smaller or equal to Y 
+  # in the population values
+  sums          <- 0
+  sumtab        <- tabpop[1]
+  
+  if(j>1){
+    for(i in 2:length(ivec)){
+      sums   <- sums + (sumtab)/M*(min(y, ivec[i])-ivec[i-1])
+      sumtab <- sumtab + tabpop[i]
+      
+    }
+  }
+  sums <- as.numeric(sums)
+  # Maximum cummulative error from -Inf  to the minimum value between y and M
+  firstTerm <- firstIntegral + sums
+  
+  # Cummulative error from M to y, if y>M
+  if(y>Xm){
+    secondTerm <- y-Xm-as.numeric(integral(Xm,y)[1])
+  }else{
+    secondTerm <- 0
+  }
+  maxAccumError <- firstTerm + secondTerm
+  return(maxAccumError)
+}
